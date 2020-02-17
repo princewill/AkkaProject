438b1a14b9112a86f4548543f28fc37f23e9e1c0
princewill ohuabunwa <wohuabun@gmail.com>
2020-02-17 18:36:47 -0500

    enqueue

--- src/main/scala/com/akka/prac/CustomMailbox.scala
+++ src/main/scala/com/akka/prac/CustomMailbox.scala
@@ -2,6 +2,7 @@ package com.akka.prac
 
 import java.util.concurrent.ConcurrentLinkedQueue
 
+import akka.actor.ActorRef
 import akka.dispatch.{Envelope, MessageQueue}
 
 object CustomMailbox {
@@ -13,6 +14,24 @@ class MyMessageQueue extends MessageQueue {
 
   private final val queue = new ConcurrentLinkedQueue[Envelope]()
 
+  def enqueue(receiver: ActorRef, handle: Envelope): Unit = {
+    if(handle.sender.path.name == "MyActor") {
+      handle.sender !  "Hey dude, How are you?, I Know your name,processing your request"
+      queue.offer(handle)
+    }
+    else handle.sender ! "I don't talk to strangers, I can't process your request"
+  }
+
+  def dequeue(): Envelope = queue.poll
+
+  def numberOfMessages: Int = queue.size
+
+  def hasMessages: Boolean = !queue.isEmpty
   
+  def cleanUp(owner: ActorRef, deadLetters: MessageQueue) {
+    while (hasMessages) {
+      deadLetters.enqueue(owner, dequeue())
+    }
+  }
 
 }
